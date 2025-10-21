4d43b57590420dae201796f2bacd261cb61f02ee
Imngzx <zhixuanng14@gmail.com>
2025-10-21 14:29:44 +0800

    huge changed to lualine and incline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -144,27 +144,27 @@ return {
       --
       -- show filetype in lualine_x
       opts.sections.lualine_x = {
-        {
-          "diagnostics",
-
-          -- Table of diagnostic sources, available sources are:
-          --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
-          -- or a function that returns a table as such:
-          --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
-
-          -- Displays diagnostics for the defined severity types
-          sections = { "error", "warn", "info", "hint" },
-          symbols = {
-            error = " ",
-            warn  = " ",
-            hint  = " ",
-            info  = " ",
-          },
-
-          colored = true,           -- Displays diagnostics status in color if set to true
-          update_in_insert = false, -- Update diagnostics in insert mode.
-          always_visible = false,   -- Show diagnostics even if there are none.
-        },
+        -- {
+        --   "diagnostics",
+        --
+        --   -- Table of diagnostic sources, available sources are:
+        --   --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+        --   -- or a function that returns a table as such:
+        --   --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+        --
+        --   -- Displays diagnostics for the defined severity types
+        --   sections = { "error", "warn", "info", "hint" },
+        --   symbols = {
+        --     error = " ",
+        --     warn  = " ",
+        --     hint  = " ",
+        --     info  = " ",
+        --   },
+        --
+        --   colored = true,           -- Displays diagnostics status in color if set to true
+        --   update_in_insert = false, -- Update diagnostics in insert mode.
+        --   always_visible = false,   -- Show diagnostics even if there are none.
+        -- },
 
         {
           "lsp_status",
@@ -231,13 +231,37 @@ return {
           end
           local ft_icon, ft_color = devicons.get_icon_color(filename)
           local modified = vim.bo[props.buf].modified
+
+          local function get_diagnostic_label()
+            local icons = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+            local label = {}
+
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+              end
+            end
+            if #label > 0 then
+              table.insert(label, { ' ' })
+            end
+            return label
+          end
+
           return {
-            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold" or "none" },
-            modified and { " [+]", guifg = "#ff9e64" } or "",
-            " ",
             guibg = "#44406e",
+            {
+              ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+              " ",
+              { filename, gui = modified and "bold" or "none" },
+              modified and { " [+]", guifg = "#ff9e64" } or "",
+              " ",
+            },
+
+            {
+              "| ",
+              get_diagnostic_label(),
+            }
           }
         end,
       })
