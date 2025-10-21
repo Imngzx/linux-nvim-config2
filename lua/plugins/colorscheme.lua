3cd15d74827671efc3517ea396eab8516c28fbc4
Imngzx <zhixuanng14@gmail.com>
2025-10-21 20:49:31 +0800

    solved many warnings in the solarizd osaka

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,6 +11,7 @@ return {
   {
     "catppuccin/nvim",
     lazy = true,
+    priority = 1000,
     name = "catppuccin",
     opts = {
       transparent_background = false,
@@ -83,6 +84,7 @@ return {
   {
     "rose-pine/neovim",
     lazy = true,
+    priority = 1000,
     name = "rose-pine",
     -- priority = 1000,
     config = function()
@@ -123,7 +125,7 @@ return {
   {
     "ellisonleao/gruvbox.nvim",
     lazy = true,
-    -- priority = 1000 ,
+    priority = 1000,
     config = function()
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
@@ -157,6 +159,7 @@ return {
   {
     "rebelot/kanagawa.nvim",
     lazy = true,
+    priority = 1000,
     config = function()
       require("kanagawa").setup({
         compile = false,  -- enable compiling the colorscheme
@@ -219,64 +222,69 @@ return {
   -- NOTE: neosolarized
   {
     "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    config = function()
-      require("solarized-osaka").setup({
-        transparent = false,
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = false },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark",              -- style for sidebars, see below
-          floats = "dark",                -- style for floating windows
-        },
-        sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false,             -- dims inactive windows
-        lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
-        on_highlights = function(hl, c)
-          local prompt = "#2d3149"
-          hl.TelescopeNormal = {
-            bg = c.bg_dark,
-            fg = c.fg_dark,
-          }
-          hl.TelescopeBorder = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.TelescopePromptNormal = {
-            bg = c.bg_dark,
-          }
-          hl.TelescopePromptBorder = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.TelescopePromptTitle = {
-            bg = prompt,
-            fg = "#2C94DD",
-          }
-          hl.TelescopePreviewTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.TelescopeResultsTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-        end,
-      })
-    end,
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = false,
+      terminal_colors = true,
+
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent",   -- style for floating windows
+        lualine_bold = true,
+      },
+
+
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+
+    },
   },
+
+
+
+
+
   -- NOTE : tokyonight
   {
     "folke/tokyonight.nvim",
     lazy = true,
+    priority = 1000,
     opts = {
       transparent = false,
       styles = {
