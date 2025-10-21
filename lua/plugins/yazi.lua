52bb1df80bfdf4ca87a3d7bc7c27c75319de59ed
Imngzx <zhixuanng14@gmail.com>
2025-10-21 13:23:36 +0800

    added yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,8 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = "folke/snacks.nvim",
+  init = function() vim.g.loaded_netrwPlugin = 1 end,
+  keys = { { "<leader>az", mode = "n", "<cmd>Yazi<cr>", desc = "Open yazi at the current file" } },
+  opts = { open_for_directories = true },
+}
