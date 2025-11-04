695e2ff79b717f3cfc72a7b670f9615752d9f1a6
Imngzx <zhixuanng14@gmail.com>
2025-11-04 23:08:35 +0800

    changed how lualine looks

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -113,12 +113,29 @@ return {
 
       opts.sections.lualine_b = {
         {
-          "branch",
-        },
+          "diagnostics",
+
+          -- Table of diagnostic sources, available sources are:
+          --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+          -- or a function that returns a table as such:
+          --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+
+          -- Displays diagnostics for the defined severity types
+          sections = { "error", "warn", "info", "hint" },
+          symbols = {
+            error = " ",
+            warn  = " ",
+            hint  = " ",
+            info  = " ",
+          },
+        }
       }
 
       opts.sections.lualine_c = {
 
+        {
+          "branch",
+        },
         {
           "diff",
           symbols = {
@@ -214,69 +231,29 @@ return {
     enabled = true,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local helpers = require("incline.helpers")
-      local devicons = require("nvim-web-devicons")
-      require("incline").setup({
+      local helpers = require 'incline.helpers'
+      local devicons = require 'nvim-web-devicons'
+      require('incline').setup {
         window = {
-          padding = 1,
-          margin = { horizontal = 1 },
-        },
-        hide = {
-          cursorline = true,
-        },
-        ignore = {
-          floating_wins = false,
-          wintypes = function(winid, wintype)
-            local zen = package.loaded['snacks'].zen
-            if zen.win and not zen.win.closed then
-              return winid ~= zen.win.win
-            end
-            return wintype ~= ''
-          end
+          padding = 0,
+          margin = { horizontal = 0 },
         },
         render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if filename == "" then
-            filename = "[No Name]"
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          if filename == '' then
+            filename = '[No Name]'
           end
           local ft_icon, ft_color = devicons.get_icon_color(filename)
           local modified = vim.bo[props.buf].modified
-
-          local function get_diagnostic_label()
-            local icons = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
-            local label = {}
-
-            for severity, icon in pairs(icons) do
-              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-
-              if n > 0 then
-                table.insert(label, { ' ' })
-                table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
-              end
-            end
-            if #label > 0 then
-              table.insert(label, { '|' })
-            end
-            return label
-          end
-
           return {
-            guibg = "#45475a",
-            {
-              ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-              { " ", guifg = ft_color, },
-              { filename, gui = modified and "bold" or "none" },
-              modified and { " [+]", guifg = "#ff9e64" } or "",
-              " ",
-            },
-
-            {
-              get_diagnostic_label(),
-            },
-
+            ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+            { " ", guifg = ft_color, },
+            { filename, gui = modified and 'bold,italic' or 'bold' },
+            ' ',
+            guibg = '#44406e',
           }
         end,
-      })
+      }
     end,
   },
 
