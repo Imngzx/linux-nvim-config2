0719ad1163ec6699b02bc358708a50de790c7fce
Imngzx <zhixuanng14@gmail.com>
2025-11-05 21:24:53 +0800

    removed line highlighting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- -- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-vim.o.cursorline = false
+-- vim.cmd([[let &t_Cs = "\e[4:3m"]])
+-- vim.cmd([[let &t_Ce = "\e[4:0m"]])
+vim.o.cursorline = true
 vim.o.termguicolors = true
+vim.opt.cursorlineopt = 'number'
